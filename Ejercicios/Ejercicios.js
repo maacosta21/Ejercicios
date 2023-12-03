df8004781ae07c44551ad3b593b20fbbf01ab52e
macosta <vacacionesvillavicencio@gmail.com>
2023-12-03 11:32:45 -0500

    Practicas

--- Ejercicios/Ejercicios.js
+++ Ejercicios/Ejercicios.js
@@ -1,6 +1,27 @@
-numeroJugador = prompt('cuanto es 2 + 2')
-    if(numeroJugador == 4) {
-        alert('Bien hecho')
-    }else {numeroJugador = prompt('cuanto es 2 + 2')}
+function powerfullgril (name, color, superpower) {
+    this.name = name
+    this.color = color
+    this.superpower = superpower
+    this.isLeader = false
+    this.displayGirls = function() {
+        console.log(`Girl information
+        nombre: ${this.name}
+        color: ${this.color}
+        poder: ${this.superpower}
+        ${this.isLeader ? 'leader: Si' : 'leader: No'}`)
+    }
     
+    this.displayleader = function () {
+            this.isLeader = true
+            console.log(`${this.name} es la lider`)
+    }
 
+
+}
+
+const blossom =  new powerfullgril('Manzanita', 'rojo', 'Fuego')
+const buttercup =  new powerfullgril('Pera', 'verde', 'tierra')
+
+blossom.displayleader()
+blossom.displayGirls()
+buttercup.displayGirls()
