b44a8bdb88ac9b8b75e86401f9cd758cd67d4324
Macosta <vacacionesvillavicencio@gmail.com>
2023-12-05 13:07:02 -0500

    Cambios terminados

--- Ejercicios/Ejercicios.js
+++ Ejercicios/Ejercicios.js
@@ -1,6 +1,28 @@
-numeroJugador = prompt('cuanto es 2 + 2')
-    if(numeroJugador == 4) {
-        alert('Bien hecho')
-    }else {numeroJugador = prompt('cuanto es 2 + 2')}
-    
+let listaNumeros = [1, 2, 3, 4, 2, 6, 0,]
+let prersultado = 0
+let arraytemporal = [[]]
+let arrayFinal = []
 
+function encontrarNumeros() {
+for (let index = 0; index < listaNumeros.length; index++) {
+    let calcular = listaNumeros[index]
+    prersultado = prersultado + calcular
+    if (prersultado <= 6) {
+        arraytemporal[0].push(calcular)
+        crearArrays()
+        console.log(arraytemporal)
+        console.log(listaNumeros[index])
+        
+    }
+    }
+    
+}
+encontrarNumeros()
+
+function crearArrays() {
+    if( prersultado == 6 ) {
+        prersultado = 0
+        arrayFinal = arrayFinal.concat(arraytemporal)
+        arraytemporal = [[]]
+    }
+}
